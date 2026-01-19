4805186d774a3a64806dfa3c9950577ae9ffd6ee
Me1onpan <me1onpan@outlook.com>
2026-01-19 22:17:25 +0800

    refactor(verina): 优化维里奈战斗循环逻辑
    - 区分有无变奏进场的连招
    - 调整共鸣技能后摇延迟为 0.35 秒
    - 执行跳跃+普攻消耗共鸣回路
    - 修正 switch_next_char 返回值

--- src/char/Verina.py
+++ src/char/Verina.py
@@ -12,14 +12,19 @@ class Verina(Healer):
 
     def do_perform(self):
         if self.has_intro:
-            self.heavy_attack(1.5)
-            return self.switch_next_char()
-        self.wait_down()
-        liberated = self.click_liberation()
-        self.click_resonance(send_click=False)
-        self.click_echo(time_out=0)
-        if self.is_mouse_forte_full():
-            self.heavy_attack()
+            self.wait_down(click=False)
+        else:
+            self.continues_normal_attack(1.1)
+        liberated = False
+        if self.liberation_available():
+            liberated = self.click_liberation()
+        if self.resonance_available():
+            self.click_resonance(send_click=False,post_sleep=0.35)
+        if self.echo_available():
+            self.click_echo()
+        if self.is_mouse_forte_full() or not self.has_intro:
+            self.task.jump(after_sleep=0.1)
+            self.continues_normal_attack(1.1)
         elif not liberated:
             self.click_liberation(wait_if_cd_ready=1)
-        self.switch_next_char()
+        return self.switch_next_char()
